0d93de63a5958e05c6b5fdace584873473b0c7d0
artvandelayio <mattmerrickbiz@gmail.com>
2025-10-15 22:05:51 -0400

    lfg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,13 +7,13 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://www.mattmerrick.com'),
-  title: 'Matt Merrick',
-  description: 'Subscribe to My Daily Newsletter | Matt Merrick',
+  title: 'A Daily Newsletter to Make You Think | Matt Merrick',
+  description: 'A daily newsletter to make you think. Subscribe for thoughtful insights, every day.',
   openGraph: {
     type: 'website',
     url: 'https://www.mattmerrick.com/',
-    title: 'Matt Merrick',
-    description: 'Subscribe to My Daily Newsletter | Matt Merrick',
+    title: 'A Daily Newsletter to Make You Think | Matt Merrick',
+    description: 'A daily newsletter to make you think.',
     images: [
       {
         url: '/share.png',
@@ -25,8 +25,8 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Matt Merrick',
-    description: 'Subscribe to My Daily Newsletter | Matt Merrick',
+    title: 'A Daily Newsletter to Make You Think | Matt Merrick',
+    description: 'A daily newsletter to make you think.',
     images: ['/share.png'],
   },
 }
